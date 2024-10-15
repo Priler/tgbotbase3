04044a1be42eb54fb042d4cb2edc2a6faaaa1032
Priler <priler96@gmail.com>
2024-10-15 22:53:42 +0500

    small bugfixes + small requirements changes for windows

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 from dispatcher import dp, bot
+import handlers
 
 async def main():
     await dp.start_polling(bot, skip_updates=False) # Don't skip updates, if your bot will process payments or other important stuff
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,7 @@
 from . import exceptions
+
+from . import admin_actions
+from . import callbacks
+from . import group_events
 from . import personal_actions
+from . import user_actions
